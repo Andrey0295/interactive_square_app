0a48bd41ee28f8c887990534c109c4ff2733fc2b
Ustinov Andrey <andreeicandrej00@gmail.com>
2021-10-14 01:48:58 +0300

    setts publish on gh pages

--- src/components/SquareMain/SquareMain.js
+++ src/components/SquareMain/SquareMain.js
@@ -1,26 +1,31 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import GameArea from "../GameArea/GameArea";
 import styles from "./SquareMain.module.css";
 
+const BASE_URL = "https://demo1030918.mockable.io/";
+const cellColorMain = "white";
+const cellColorHover = "blue";
+
+const { gameMenuBlock, select, gameBtn } = styles;
+
 const MainSquare = () => {
-  let [size, setSize] = useState(0);
-  let [matrix, setMatrix] = useState([]);
-  let [cellColor] = useState("blue");
-  let [hoverHistory, setHoverHistory] = useState([]);
-  let [squareMode, setSquareMode] = useState(null);
+  const [size, setSize] = useState(0);
+  const [matrix, setMatrix] = useState([]);
+  const [hoverHistory, setHoverHistory] = useState([]);
+  const [squareMode, setSquareMode] = useState({});
+  const [isStart, setIsStart] = useState(false);
+  const [selected, setSelected] = useState("pick mode");
 
   useEffect(
-    () =>
-      axios
-        .get("http://demo1030918.mockable.io/")
-        .then((res) => setSquareMode(res.data)),
+    () => axios.get(BASE_URL).then(({ data }) => setSquareMode(data)),
     []
   );
 
   useEffect(() => {
     let row = [];
     let col = [];
-    for (let index = 0; index < size; index++) {
+    for (let i = 0; i < size; i++) {
       col.push(0);
       row.push(col);
     }
@@ -30,51 +35,70 @@ const MainSquare = () => {
   function onMouseOver(event) {
     const { style, dataset } = event.currentTarget;
 
-    style.backgroundColor === cellColor
-      ? (style.backgroundColor = "white")
-      : (style.backgroundColor = cellColor);
-    setHoverHistory([...hoverHistory, dataset.text.split()]);
+    if (style.backgroundColor === cellColorHover) {
+      style.backgroundColor = cellColorMain;
+    } else {
+      style.backgroundColor = cellColorHover;
+      setHoverHistory([...hoverHistory, dataset.text.split()]);
+    }
   }
 
   function onChangeSelect(e) {
-    setSize(squareMode[e.target.value].field);
+    const { value } = e.target;
+    setSize(squareMode[value].field);
+    setSelected(value);
+  }
+
+  function onButtonClick() {
+    setIsStart(!isStart);
+    if (isStart) {
+      resetCurrentLevel();
+    }
+  }
+
+  function resetCurrentLevel() {
+    setSize(0);
+    setMatrix([]);
+    setHoverHistory([]);
+    setSelected("pick mode");
   }
 
   return (
     <>
-      {squareMode && (
-        <select onChange={onChangeSelect} defaultValue={"pick mode"}>
-          <option disabled value={"pick mode"}>
-            pick mode
-          </option>
-          {Object.keys(squareMode).map((opt) => (
-            <option key={opt} value={opt}>
-              {opt}
+      <div className={gameMenuBlock}>
+        <button
+          style={isStart ? { backgroundColor: "maroon" } : null}
+          className={gameBtn}
+          onClick={onButtonClick}
+          disabled={size ? false : true}
+        >
+          {isStart ? "Stop" : "Start"}
+        </button>
+        {squareMode && (
+          <select
+            className={select}
+            onChange={onChangeSelect}
+            value={selected}
+            disabled={isStart ? true : false}
+          >
+            <option hidden value={"pick mode"}>
+              pick mode
             </option>
-          ))}
-        </select>
-      )}
-      <div className={styles.square_block}>
-        <ul>
-          {matrix.map((el, i) => (
-            <li className={styles.row} key={i}>
-              {el.map((sp, idx) => (
-                <div
-                  className={styles.col}
-                  key={idx}
-                  onMouseOver={onMouseOver}
-                  data-text={`row ${i + 1}, col ${idx + 1}`}
-                ></div>
-              ))}
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {hoverHistory.map((el, index) => (
-            <li key={index}>{el}</li>
-          ))}
-        </ul>
+            {Object.keys(squareMode).map((opt) => (
+              <option key={opt} value={opt}>
+                {opt}
+              </option>
+            ))}
+          </select>
+        )}
       </div>
+      <GameArea
+        isStart={isStart}
+        gameMode={squareMode}
+        squareMatrixBuilder={matrix}
+        onMouseOver={onMouseOver}
+        hoverHistory={hoverHistory}
+      />
     </>
   );
 };
